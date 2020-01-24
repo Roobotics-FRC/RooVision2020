4ca997a103ebb044e97fdd94898c674b1143828b
aaplmath <aaplmath@users.noreply.github.com>
2020-01-24 16:05:27 -0500

    Add infrastructure for focal length computations

--- src/main/java/RooProcessor.java
+++ src/main/java/RooProcessor.java
@@ -4,8 +4,15 @@ import edu.wpi.first.vision.VisionThread;
 import org.opencv.core.Rect;
 import org.opencv.imgproc.Imgproc;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class RooProcessor {
     private static final double TARGET_WIDTH_INCHES = 39.25;
+    private static final String FOCAL_LENGTH_CONFIG_PATH = "/boot/focal_length.txt";
+
+    private double focalLength = -1;
 
     private VideoSource camera;
     private NetworkTable visionTable;
@@ -13,6 +20,7 @@ public class RooProcessor {
     public RooProcessor(VideoSource camera, NetworkTable visionTable) {
         this.camera = camera;
         this.visionTable = visionTable;
+        readFocalLength();
     }
 
     public void process() {
@@ -33,5 +41,50 @@ public class RooProcessor {
             }
         });
         visionThread.start();
+    }
+
+    /**
+     * Reads the saved focal length from disk and stores in the {@link #focalLength} variable.
+     */
+    private void readFocalLength() {
+        try {
+            String rawSave = Files.readString(Paths.get(FOCAL_LENGTH_CONFIG_PATH));
+            this.focalLength = Double.parseDouble(rawSave);
+        } catch (IOException e) {
+            System.out.println("Warning: No saved focal length found." +
+                    " This must be computed for distance measurement to work.");
+        } catch (Exception e) {
+            System.err.println("Illegal data found when trying to read saved focal length");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Writes the currently computed focal length to disk.
+     */
+    private void writeFocalLength() {
+        try {
+            Files.writeString(Paths.get(FOCAL_LENGTH_CONFIG_PATH), Double.toString(this.focalLength));
+        } catch (IOException e) {
+            System.err.println("Failed to write focal length to disk:");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Computes the focal length based on the perceived contour width at a known distance specified
+     * in NetworkTables in the <code>fl_calibration_distance</code> field. Saves in
+     * {@link #focalLength} and writes to disk.
+     *
+     * @param perceivedWidthPx the width of the contour at the calibration distance, in px.
+     */
+    private void computeFocalLength(double perceivedWidthPx) {
+        double knownDist = visionTable.getEntry("fl_calibration_distance").getDouble(-1);
+        if (knownDist <= 0) {
+            System.out.println("Invalid or missing fl_calibration_distance. Computation aborted.");
+            return;
+        }
+        this.focalLength = knownDist * perceivedWidthPx / TARGET_WIDTH_INCHES;
+        writeFocalLength();
     }
 }
